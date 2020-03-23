5b179348ab5c2d43896f890e971fe2fa994ee2e1
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-03-23 12:15:32 +0200

    kokeillaan formia uudelleen

--- application/results/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-
-class ResultForm(FlaskForm):
-    description = StringField("description")
-
-    class Meta:
-        csrf = False
--- application/results/views.py
+++ application/results/views.py
@@ -1,7 +1,6 @@
 from application import app, db
 from flask import redirect, render_template, request, url_for
 from application.results.models import Result
-from application.results.forms import ResultForm
 
 @app.route("/results", methods=["GET"])
 def results_index():
@@ -9,16 +8,20 @@ def results_index():
 
 @app.route("/results/new")
 def results_form():
-    return render_template("results/new.html", form=ResultForm)
+    return render_template("results/new.html")
 
 @app.route("/results/", methods=["POST"])
 def results_create():
-    r = Result(request.form.get("description"))
+    r = Result(request.form.get("name"))
 
     db.session().add(r)
     db.session().commit()
 
     return redirect(url_for("results_index"))
+
+@app.route("/results/modify/<result_id>")
+def results_modify(result_id):
+    return render_template("results/modify.html", result = Result.query.get(result_id))
 
 @app.route("/results/<result_id>", methods=["GET"])
 def results_delete(result_id):
@@ -29,9 +32,6 @@ def results_delete(result_id):
 
     return redirect(url_for("results_index"))
 
-@app.route("/results/modify/<result_id>")
-def results_modify(result_id):
-    return render_template("results/modify.html", result = Result.query.get(result_id))
 
 @app.route("/results/savemodified/<result_id>", methods=["POST"])
 def results_savemodified(result_id):
