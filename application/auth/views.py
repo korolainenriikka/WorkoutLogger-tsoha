5d3c167d6c4549caff0c053bd11ddb6b13070ee0
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-03-23 15:27:51 +0200

    uusia tiedostoja

---
+++ application/auth/views.py
@@ -0,0 +1,37 @@
+from flask import render_template, request, redirect, url_for
+
+from application import app,db
+from application.auth.models import User
+from application.auth.forms import LoginForm, RegisterForm
+
+@app.route("/auth/login", methods = ["GET", "POST"])
+def auth_login():
+    if request.method == "GET":
+        return render_template("auth/loginform.html", form = LoginForm())
+
+    form = LoginForm(request.form)
+    # mahdolliset validoinnit
+
+    user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
+    if not user:
+        return render_template("auth/loginform.html", form = form,
+                               error = "No such username or password")
+
+
+    print("Käyttäjä " + user.name + " tunnistettiin")
+    return redirect(url_for("index"))
+
+@app.route("/auth/register", methods = ["GET", "POST"])
+def auth_register():
+    if request.method == "GET":
+        return render_template("auth/registerform.html", form = RegisterForm())
+
+    form = RegisterForm(request.form)
+    # mahdolliset validoinnit
+
+    user = User(form.name, form.username, form.password)
+
+    db.session().add(user)
+    db.session().commit()
+
+    return redirect(url_for("index"))
