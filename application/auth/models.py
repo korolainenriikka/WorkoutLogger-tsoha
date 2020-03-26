0219b3d20df3e6d08f8127db4646e00b2e0126e2
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-03-26 17:09:02 +0200

    bugfix

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,5 +1,5 @@
 from application import db
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash
 
 
 class User(db.Model):
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user, current_user, login_required
-from werkzeug import check_password_hash
+from werkzeug.security import check_password_hash
 
 from application import app, db
 from application.auth.models import User
