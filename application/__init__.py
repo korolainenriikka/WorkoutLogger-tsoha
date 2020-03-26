f7dde25e156a63fa41e75feb87c0966b169a92f4
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-03-26 11:46:49 +0200

    init.py järjestetty ja lisätty loginmanager

--- application/__init__.py
+++ application/__init__.py
@@ -1,21 +1,41 @@
+# flask-sovellus
 from flask import Flask
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-
 app = Flask(__name__)
 
+# tietokanta
+from flask_sqlalchemy import SQLAlchemy
 import os
-
 if os.environ.get("HEROKU"):
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 else:
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///wologger.db"
     app.config["SQLALCHEMY_ECHO"] = True
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+# migraatio
+from flask_migrate import Migrate
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 migrate = Migrate(app, db)
 
+# kirjautuminen
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to view this functionality"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+# oma sovellus
 from application import views
 
 from application.results import models
@@ -24,4 +44,5 @@ from application.results import views
 from application.auth import models
 from application.auth import views
 
+#luodaan tietokantataulut
 db.create_all()
