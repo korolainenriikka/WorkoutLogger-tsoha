5bd1df8f263ad82f1445fd612822639ed0a4596d
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-04-06 14:31:11 +0300

    user stats admin-requirementilla

--- application/analyze/views.py
+++ application/analyze/views.py
@@ -4,14 +4,22 @@ from flask_login import login_required, current_user
 from application import app, admin_permission
 from application.models import Result, Session
 
+
 @app.route("/analyze/", methods=["GET"])
 @login_required
 def list_recent():
-    results = Result.query.join(Session).filter_by(account_id=current_user.id).all()
-    return render_template("analyze/list.html", results = results)
+	results = Result.query.join(Session).filter_by(account_id=current_user.id).all()
+	return render_template("analyze/list.html", results=results)
+
 
 @app.route("/analyze/showactivity", methods=["GET"])
 @login_required
-@admin_permission.require()
 def show_activity():
+	return render_template("analyze/activity.html", count=Session.count_sessions_last_30_days())
+
+@app.route("/analyze/listusers", methods=["GET"])
+@login_required
+@admin_permission.require()
+def list_users():
     return render_template("analyze/activity.html", count=Session.count_sessions_last_30_days())
+
